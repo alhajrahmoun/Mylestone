34e3b2ea8d91131462965b982539125ede220d70
mhdrahmoun <mrahmoun93@gmail.com>
2019-03-03 01:09:07 +0200

    add capistrano

---
+++ Capfile
@@ -0,0 +1,15 @@
+# Load DSL and Setup Up Stages
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+require 'capistrano/rvm'
+require "capistrano/bundler"
+require "capistrano/rails/assets"
+require "capistrano/rails/migrations"
+require "capistrano/passenger"
+
+
+# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "mylestone"
+set :repo_url, "git@github.com:mhdrahmoun/Mylestone.git"
+
+set :deploy_to, '/home/mylestone/apps/mylestone'
+
+set :keep_releases, 2
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
