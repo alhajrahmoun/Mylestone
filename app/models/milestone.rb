56b54165310ce63698e7bd805eb12df748a2cd39
Mohamad Alhaj Rahmoun <mrahmoun93@gmail.com>
2019-04-06 17:51:28 +0800

    remove scope open

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -2,9 +2,7 @@ class Milestone < ApplicationRecord
   enum status: [:open, :secret]
   belongs_to :user
   validate :date_not_in_future, on: :create
-
-  scope :open, -> {where(status: :open)}
-
+  
   def date_not_in_future
     logger.info self.inspect
     errors.add('error:', "Milestone date can't be in the future") if self.date > Date.today
