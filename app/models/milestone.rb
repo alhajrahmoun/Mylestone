ea6f8d0aed0543bf41b0b79b9a80608335036fba
Mohamad Alhaj Rahmoun <mrahmoun93@gmail.com>
2019-04-11 03:03:30 +0800

    use date.current

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -4,6 +4,6 @@ class Milestone < ApplicationRecord
   validate :date_not_in_future, on: :create
 
   def date_not_in_future
-    errors.add('error:', "Milestone date can't be in the future") if self.date > Date.today
+    errors.add('error:', "Milestone date can't be in the future") if self.date > Date.current
   end
 end
