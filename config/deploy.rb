e43c40778999ed94a161d0877a5a1cc55f0074a8
Mohamad Alhaj Rahmoun <mrahmoun93@gmail.com>
2020-06-07 23:08:51 +0800

    update capistrano loaded version to 3.14.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.13.0"
+lock "~> 3.14.0"
 
 set :application, "mylestone"
 set :repo_url, "git@github.com:mhdrahmoun/Mylestone.git"
